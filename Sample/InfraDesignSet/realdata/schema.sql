e8fd4861c14cfa1e6ddd87abaf2f323c57e2dc19
SNChanger <sakuriver.redmine@gmail.com>
2023-07-15 18:31:17 +0900

    基幹システム関連をアップ
    ・位置情報基盤関連のデータ構造（お散歩アプリや位置情報ゲームなどでの想定）

---
+++ Sample/InfraDesignSet/realdata/schema.sql
@@ -0,0 +1,24 @@
+-- base_spot_data
+CREATE TABLE base_spot_data 
+( 
+  id varchar(255) PRIMARY KEY, 
+  app_id int(10), 
+  body varchar(255),
+
+  name varchar(255),
+  body varchar(255),
+ created_datetime datetime,
+ updated_datetime datetime
+)
+ENGINE=INNODB;
+
+
+CREATE TABLE base_log_data
+( 
+  id varchar(255) PRIMARY KEY, 
+  app_id int(10), 
+  created_datetime datetime,
+  updated_datetime datetime
+)
+ENGINE=INNODB;
+
